8a7748e3afb8534553304a70d5a7881558f1cea3
lucas <lucas@llnv>
2016-11-10 17:19:32 -0200

    Busca e exibição do arquivo CSV na tabela

--- src/svacee/ctrl/DadoConsumoCtrl.java
+++ src/svacee/ctrl/DadoConsumoCtrl.java
@@ -1,35 +1,67 @@
 package svacee.ctrl;
 // @author Ana Letícia, Lucas Diego e Matheus Quilles
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import svacee.entity.DadoConsumo;
 
 public class DadoConsumoCtrl {
 
-    private Timestamp dataHora = new Timestamp(System.currentTimeMillis());
-    private String idPontoColeta;
-    private double valorKwH;
+    private File arq;
+    private List<DadoConsumo> dados = new ArrayList<>();
 
-    public Timestamp getDataHora() {
-        return dataHora;
+    public void obterDadosCSV(File arq) throws FileNotFoundException, IOException {
+
+        setArq(arq);
+
+        String linha = "";
+        String[] linhas;
+        String[] coluna;
+
+        BufferedReader br = null;
+        FileReader fr = new FileReader(arq);
+        
+        br = new BufferedReader(fr);
+
+        DadoConsumo dc;
+
+        while ((linha = br.readLine()) != null) {
+            linhas = linha.split(";");
+
+            for (String v : linhas) {
+                coluna = v.split(",");
+
+                dc = new DadoConsumo();
+
+                dc.setDataHora(Timestamp.valueOf(coluna[0]));
+                dc.setIdPontoColeta(coluna[1]);
+                dc.setValorKwH(Double.parseDouble(coluna[2]));
+
+                getDados().add(dc);
+            }
+
+        }
     }
 
-    public void setDataHora(Timestamp dataHora) {
-        this.dataHora = dataHora;
+    public File getArq() {
+        return arq;
     }
 
-    public String getIdPontoColeta() {
-        return idPontoColeta;
+    public void setArq(File arq) {
+        this.arq = arq;
     }
 
-    public void setIdPontoColeta(String idPontoColeta) {
-        this.idPontoColeta = idPontoColeta;
+    public List<DadoConsumo> getDados() {
+        return dados;
     }
 
-    public double getValorKwH() {
-        return valorKwH;
-    }
-
-    public void setValorKwH(double valorKwH) {
-        this.valorKwH = valorKwH;
+    public void setDados(List<DadoConsumo> dados) {
+        this.dados = dados;
     }
 }
--- src/svacee/entity/DadoConsumo.java
+++ src/svacee/entity/DadoConsumo.java
@@ -1,6 +1,39 @@
 package svacee.entity;
 // @author Ana Letícia, Lucas Diego e Matheus Quilles
 
+import java.sql.Timestamp;
+
 public class DadoConsumo {
 
+    private Timestamp dataHora;
+    private String idPontoColeta;
+    private double valorKwH;
+    
+    public DadoConsumo(){
+        
+    }
+
+    public Timestamp getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(Timestamp dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getIdPontoColeta() {
+        return idPontoColeta;
+    }
+
+    public void setIdPontoColeta(String idPontoColeta) {
+        this.idPontoColeta = idPontoColeta;
+    }
+
+    public double getValorKwH() {
+        return valorKwH;
+    }
+
+    public void setValorKwH(double valorKwH) {
+        this.valorKwH = valorKwH;
+    }
 }
